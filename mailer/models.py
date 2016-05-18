e043c042b8f7c8010f16fff99ad5f62246b12b30
Bob Cribbs <bob@rewards.ro>
2016-05-18 19:15:53 +0300

    refs #3 add index on priority and when_added

--- mailer/models.py
+++ mailer/models.py
@@ -17,21 +17,21 @@ PRIORITIES = (
 
 
 class MessageManager(models.Manager):
-    
+
     def non_deferred(self):
         """
         the messages in the queue not deferred
         """
-        
+
         return self.filter(priority__lt="4")
-    
+
     def deferred(self):
         """
         the deferred messages in the queue
         """
-    
+
         return self.filter(priority="4")
-    
+
     def retry_deferred(self, new_priority=2):
         count = 0
         for message in self.deferred():
@@ -61,23 +61,23 @@ def db_to_email(data):
 
 
 class Message(models.Model):
-    
+
     # The actual data - a pickled EmailMessage
     message_data = models.TextField()
     when_added = models.DateTimeField(default=datetime.now)
     priority = models.CharField(max_length=1, choices=PRIORITIES, default="2")
     # @@@ campaign?
     # @@@ content_type?
-    
+
     objects = MessageManager()
 
-	## Suggested index, very useful to the send queue:
-	## CREATE INDEX mailer_message_send_order ON mailer_message (priority, when_added) WHERE priority < '4';
-    
+    class Meta:
+        index_together = ["priority", "when_added"]
+
     def defer(self):
         self.priority = "4"
         self.save()
-    
+
     def retry(self, new_priority=2):
         if self.priority == "4":
             self.priority = new_priority
@@ -85,17 +85,17 @@ class Message(models.Model):
             return True
         else:
             return False
-    
+
     def _get_email(self):
         return db_to_email(self.message_data)
-    
+
     def _set_email(self, val):
         self.message_data = email_to_db(val)
 
     email = property(_get_email, _set_email, doc=
                      """EmailMessage object. If this is mutated, you will need to
 set the attribute again to cause the underlying serialised data to be updated.""")
-    
+
     @property
     def to_addresses(self):
         email = self.email
@@ -103,7 +103,7 @@ set the attribute again to cause the underlying serialised data to be updated.""
             return email.to
         else:
             return []
-    
+
     @property
     def subject(self):
         email = self.email
@@ -130,29 +130,29 @@ def make_message(subject="", body="", from_email=None, to=None, bcc=None,
     """
     Creates a simple message for the email parameters supplied.
     The 'to' and 'bcc' lists are filtered using DontSendEntry.
-    
+
     If needed, the 'email' attribute can be set to any instance of EmailMessage
     if e-mails with attachments etc. need to be supported.
-    
+
     Call 'save()' on the result when it is ready to be sent, and not before.
     """
     to = filter_recipient_list(to)
     bcc = filter_recipient_list(bcc)
     core_msg = EmailMessage(subject=subject, body=body, from_email=from_email,
                             to=to, bcc=bcc, attachments=attachments, headers=headers)
-    
+
     db_msg = Message(priority=priority)
     db_msg.email = core_msg
     return db_msg
 
 
 class DontSendEntryManager(models.Manager):
-    
+
     def has_address(self, address):
         """
         is the given address on the don't send list?
         """
-        
+
         queryset = self.filter(to_address__iexact=address)
         try:
             # Django 1.2
@@ -163,14 +163,14 @@ class DontSendEntryManager(models.Manager):
 
 
 class DontSendEntry(models.Model):
-    
+
     to_address = models.EmailField()
     when_added = models.DateTimeField()
     # @@@ who added?
     # @@@ comment field?
-    
+
     objects = DontSendEntryManager()
-    
+
     class Meta:
         verbose_name = "don't send entry"
         verbose_name_plural = "don't send entries"
@@ -185,13 +185,13 @@ RESULT_CODES = (
 
 
 class MessageLogManager(models.Manager):
-    
+
     def log(self, message, result_code, log_message=""):
         """
         create a log entry for an attempt to send the given message and
         record the given result and (optionally) a log message
         """
-        
+
         return self.create(
             message_data = message.message_data,
             when_added = message.when_added,
@@ -203,24 +203,24 @@ class MessageLogManager(models.Manager):
 
 
 class MessageLog(models.Model):
-    
+
     # fields from Message
     message_data = models.TextField()
     when_added = models.DateTimeField()
     priority = models.CharField(max_length=1, choices=PRIORITIES)
     # @@@ campaign?
-    
+
     # additional logging fields
     when_attempted = models.DateTimeField(default=datetime.now, db_index=True)
     result = models.CharField(max_length=1, choices=RESULT_CODES)
     log_message = models.TextField()
-    
+
     objects = MessageLogManager()
-    
+
     @property
     def email(self):
         return db_to_email(self.message_data)
-    
+
     @property
     def to_addresses(self):
         email = self.email
@@ -228,7 +228,7 @@ class MessageLog(models.Model):
             return email.to
         else:
             return []
-    
+
     @property
     def subject(self):
         email = self.email
